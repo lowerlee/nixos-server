9cb7f64b0a1b0d11f26cecb26c1db7bcf1871651
lowerlee <minleekevin@gmail.com>
2024-12-19 13:26:34 -0800

    tailscale

--- modules/tailscale.nix
+++ modules/tailscale.nix
@@ -4,9 +4,12 @@
   services.tailscale = {
     enable = true;
     useRoutingFeatures = "both";
+    permitCertUid = "k";
     extraUpFlags = [
+      "--reset"
       "--exit-node=us-nyc-wg-301.mullvad.ts.net"
       "--exit-node-allow-lan-access=true"
+      "--accept-routes=true"
     ];
   };
 }
