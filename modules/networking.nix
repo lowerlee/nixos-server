438c83566240502eaa8a4c0a3e66b3965ade6b9d
lowerlee <minleekevin@gmail.com>
2024-12-19 13:20:32 -0800

    tailscale

--- modules/networking.nix
+++ modules/networking.nix
@@ -2,11 +2,12 @@
 
 {
   networking = {
-    hostName = "nixos-server";
+    hostName = "nixos";
     networkmanager.enable = true;
     firewall = {
       checkReversePath = "loose";
-      allowedTCPPorts = [ 8384 ];
+      allowedTCPPorts = [ 8384 22000 ];
+      allowedUDPPorts = [ 22000 21027 ];
       trustedInterfaces = [ "tailscale0" ];
       enable = true;
     };
--- modules/system.nix
+++ modules/system.nix
@@ -56,6 +56,7 @@
   environment.systemPackages = with pkgs; [
     vim
     tree
+    tailscale
   ];
 
   services.openssh.enable = true;
