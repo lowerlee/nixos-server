1859232ab51abc422128a28a0e572c0a9552357d
lowerlee <minleekevin@gmail.com>
2024-12-19 12:20:27 -0800

    syncthing tailscale

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -14,12 +14,12 @@
       };
       devices = {
         "pixel9-pro-xl" = {
-          id = "25E6N42-MJMW4G4-GYOUDGC-55GPVZK-MTFEJID-YC3QW5A-UZO7Q65-W4L23QC"
-          addresses = [ "tcp://100.106.130.14:22000" "quic://100.106.130.14:22000" ]
+          id = "25E6N42-MJMW4G4-GYOUDGC-55GPVZK-MTFEJID-YC3QW5A-UZO7Q65-W4L23QC";
+          addresses = [ "tcp://100.106.130.14:22000" "quic://100.106.130.14:22000" ];
         };
         "nixos-desktop" = {
           id = "JVJWMW6-YOQPXH2-WKQRKYN-QPFRZGX-LO52ONW-2B46SY7-JX63A6S-WNRHAQQ";
-          addresses = [ "tcp://100.90.112.73:22000" "quic://100.90.112.73:22000" ]
+          addresses = [ "tcp://100.90.112.73:22000" "quic://100.90.112.73:22000" ];
         };  
       };
       folders = {
