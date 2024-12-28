3bfb41586fc25cc9cb4fd57fb4a7b08ac07c6f3e
lowerlee <minleekevin@gmail.com>
2024-12-28 13:18:22 -0800

    /mnt/media config

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
 
   outputs = inputs@{ self, nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
-      "nixos" = nixpkgs.lib.nixosSystem {
+      "nixos-server" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
