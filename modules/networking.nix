e5313c54e2b22d5e8eb1f7959b478afa492926d1
lowerlee <minleekevin@gmail.com>
2024-12-28 13:58:50 -0800

    qbittorrent config v2

--- modules/networking.nix
+++ modules/networking.nix
@@ -6,8 +6,8 @@
     networkmanager.enable = true;
     firewall = {
       checkReversePath = "loose";
-      allowedTCPPorts = [ 8384 22000 ];
-      allowedUDPPorts = [ 22000 21027 ];
+      allowedTCPPorts = [ 8384 22000 8080 8999 ];
+      allowedUDPPorts = [ 22000 21027 8999 ];
       trustedInterfaces = [ "tailscale0" ];
       enable = true;
     };
--- modules/qbittorrent.nix
+++ modules/qbittorrent.nix
@@ -10,11 +10,13 @@
       Type = "simple";
       User = "k";
       Group = "users";
-      ExecStart = "${pkgs.qbittorrent-nox}/bin/qbittorrent-nox --webui-port=8080";
+      ExecStart = ''
+        ${pkgs.qbittorrent-nox}/bin/qbittorrent-nox \
+          --webui-port=8080 \
+          --username=admin \
+          --password=Xfy@R!CKqx9qFTiAZh4%QC#bK64uXe%2Le*m
+      '';
       Restart = "on-failure";
     };
   };
-
-  networking.firewall.allowedTCPPorts = [ 8080 8999 ];
-  networking.firewall.allowedUDPPorts = [ 8999 ];
 }
