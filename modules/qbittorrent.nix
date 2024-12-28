8f424e72753dc765b8dc6415f2992da958c0563a
lowerlee <minleekevin@gmail.com>
2024-12-28 13:43:06 -0800

    qbittorrent config v1

--- modules/qbittorrent.nix
+++ modules/qbittorrent.nix
@@ -1,24 +1,14 @@
-{ config, lib, pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
-  # Enable qBittorrent service
   services.qbittorrent = {
     enable = true;
-    # Run as a specific user (you might want to create this user in users.nix)
-    user = "qbittorrent";
-    group = "qbittorrent";
-    
-    # Configure web interface
-    openWebInterface = true;
-    port = 8080;  # Web interface port
-    
-    # Data directory
-    dataDir = "/var/lib/qbittorrent";
-    
-    # Configure settings
-    openFirewall = true;  # This will open the webui port in the firewall
-    
-    # Additional configuration options can be set in the config file
-    configDir = "/var/lib/qbittorrent/.config/qBittorrent";
+    dataDir = "/mnt/media";
+    openFirewall = true;
+    port = 8999;
+    webPort = 8080;
+    user = "k";
+    group = "users";
+    openFirewall = true;
   };
 }
