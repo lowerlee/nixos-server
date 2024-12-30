5a427568ce4bc6bc6ba7a3cb4f27293027dec98e
lowerlee <minleekevin@gmail.com>
2024-12-29 16:48:24 -0800

    rss-bridge docker config

--- modules/tailscale.nix
+++ modules/tailscale.nix
@@ -7,6 +7,21 @@
     extraUpFlags = [
       "--exit-node=us-nyc-wg-301.mullvad.ts.net"
       "--exit-node-allow-lan-access=true"
+      "--advertise-exit-node"
+      "--accept-dns=false"
     ];
+  };
+
+  # Enable Funnel
+  systemd.services.tailscale-funnel = {
+    description = "Tailscale Funnel for RSS-Bridge";
+    after = [ "network-online.target" "tailscale.service" ];
+    wants = [ "network-online.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = "${pkgs.tailscale}/bin/tailscale serve 3000";
+    };
   };
 }
