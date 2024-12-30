b4f48e4ed284b2d79a3f0ea7ba2cd03cd105e92a
lowerlee <minleekevin@gmail.com>
2024-12-30 15:14:41 -0800

    adjusted rss-bridge config to use private fork

--- modules/rss-bridge.nix
+++ modules/rss-bridge.nix
@@ -3,6 +3,7 @@
 {
   systemd.tmpfiles.rules = [
     "d /etc/rss-bridge/config 0755 root root"
+    "d /var/lib/rss-bridge 0755 root root"  # Directory for build context
   ];
 
   systemd.services.rss-bridge = {
@@ -13,20 +14,25 @@
     after = [ "docker.service" "network.target" ];
 
     preStart = ''
+      # Clone/update the repository
+      if [ ! -d /var/lib/rss-bridge/src ]; then
+        ${pkgs.git}/bin/git clone https://github.com/lowerlee/rss-bridge.git /var/lib/rss-bridge/src
+      else
+        cd /var/lib/rss-bridge/src && ${pkgs.git}/bin/git pull
+      fi
+
+      # Always rebuild the image with latest code
+      ${pkgs.docker}/bin/docker build -t rss-bridge:latest /var/lib/rss-bridge/src
       ${pkgs.docker}/bin/docker rm -f rss-bridge || true
     '';
 
     script = ''
-      if ! ${pkgs.docker}/bin/docker image inspect rss-bridge >/dev/null 2>&1; then
-        ${pkgs.docker}/bin/docker build -t rss-bridge https://github.com/lowerlee/rss-bridge.git#master
-      fi
-
       exec ${pkgs.docker}/bin/docker run \
         --rm \
         --name rss-bridge \
         --publish 3000:80 \
         --volume /etc/rss-bridge/config:/config:Z \
-        rss-bridge
+        rss-bridge:latest
     '';
 
     postStop = ''
