1f8673ad0ea5b8006938fbfdb5a469531704575f
lowerlee <minleekevin@gmail.com>
2025-01-03 22:57:59 -0800

    initial miniflux config

--- modules/miniflux.nix
+++ modules/miniflux.nix
@@ -3,6 +3,20 @@
 {
   services.miniflux = {
     enable = true;
-    adminCredentialsFile = "/etc/miniflux.env";
+    createDatabaseLocally = true;
+    adminCredentialsFile = "/etc/miniflex.env";
+
+    config = {
+      LISTEN_ADDR = "0.0.0.0:8082";
+      BASE_URL = "http://100.69.173.61:8082";
+      
+      POLLING_FREQUENCY = "60";
+      BATCH_SIZE = "100";
+      WORKER_POOL_SIZE = "5";
+      
+      CLEANUP_FREQUENCY_HOURS = "24";
+      CLEANUP_ARCHIVE_READ_DAYS = "60";
+      CLEANUP_ARCHIVE_UNREAD_DAYS = "180";
+    };
   };
 }
