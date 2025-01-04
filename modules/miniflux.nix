d88b4b2857c7f221930ee9f1b74644f56368f51e
lowerlee <minleekevin@gmail.com>
2025-01-03 23:19:36 -0800

    added admin configs for miniflux

--- modules/miniflux.nix
+++ modules/miniflux.nix
@@ -4,9 +4,12 @@
   services.miniflux = {
     enable = true;
     createDatabaseLocally = true;
-    adminCredentialsFile = "/etc/miniflux";
 
     config = {
+      CREATE_ADMIN = "1";
+      ADMIN_USERNAME = "k";
+      ADMIN_PASSWORD = "k";
+      
       LISTEN_ADDR = "0.0.0.0:8082";
       BASE_URL = "http://100.69.173.61:8082";
       
@@ -19,10 +22,6 @@
       CLEANUP_ARCHIVE_UNREAD_DAYS = "180";
       
       LOG_LEVEL = "info";
-
-      CREATE_ADMIN = "1";
-      ADMIN_USERNAME = "k";
-      ADMIN_PASSWORD = "k";
     };
   };
 }
