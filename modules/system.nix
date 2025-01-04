1390eebc264cf6fd5c14bf47739c8f8fac965c92
lowerlee <minleekevin@gmail.com>
2025-01-04 11:19:12 -0800

    created miniflux credentials directory

--- modules/system.nix
+++ modules/system.nix
@@ -56,6 +56,7 @@
     "d /mnt/media 0777 k users"
     "d /mnt/media/shows 0777 k users"
     "d /mnt/media/movies 0777 k users"
+    "d /var/lib/miniflux 0755 miniflux miniflux -"
     "f /var/lib/miniflux/admin-credentials 0600 miniflux miniflux - ADMIN_USERNAME=k\nADMIN_PASSWORD=k"
   ];
 
