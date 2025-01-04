54d74c209f0a0e76f2943663ccf4f03c4b834af0
lowerlee <minleekevin@gmail.com>
2025-01-03 22:51:39 -0800

    initial miniflux config

--- modules/miniflux.nix
+++ modules/miniflux.nix
@@ -4,5 +4,11 @@
   services.miniflux = {
     enable = true;
     createDatabaseLocally = true;
+
+    config = {
+      LISTEN_ADDR = "0.0.0.0:8082";
+      BASE_URL = "http://100.69.173.61:8082";
+      CREATE_ADMIN = "0";
+    };
   };
 }
