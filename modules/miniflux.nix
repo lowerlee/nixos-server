96e9769620eadd85b01fbc285bd0966dfbf5ce73
lowerlee <minleekevin@gmail.com>
2025-01-04 23:35:49 -0800

    new miniflux config

--- modules/miniflux.nix
+++ modules/miniflux.nix
@@ -9,7 +9,7 @@
       LISTEN_ADDR = "0.0.0.0:8082";
       BASE_URL = "http://100.69.173.61:8082";
       
-      CREATE_ADMIN = 0;
+      CREATE_ADMIN = 1;
       ADMIN_USERNAME = "admin";
       ADMIN_PASSWORD = "adminadmin";
       
