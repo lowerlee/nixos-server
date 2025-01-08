bc4afa28aaefc81fe3664086aa10921d65fc1a9b
lowerlee <minleekevin@gmail.com>
2025-01-07 20:55:32 -0800

    Initial monitoring config

--- modules/monitor.nix
+++ modules/monitor.nix
@@ -11,17 +11,16 @@
           enable = true;
           enabledCollectors = [
             "systemd"
-            "cpu"
+            "cpufreq"  # Changed from "cpu"
             "diskstats"
             "filesystem"
             "loadavg"
             "meminfo"
             "netdev"
-            "temperature"
+            "thermal_zone"  # Changed from "temperature"
           ];
           port = 9100;
-          openFirewall = true;  # Add this line
-          listenAddress = "100.69.173.61";  # Add this line
+          openFirewall = true;
         };
       };
 
@@ -51,7 +50,7 @@
       enable = true;
       settings = {
         server = {
-          http_port = 3001;  # Changed from port = 3001
+          http_port = 3001;
           domain = "100.69.173.61";
           root_url = "http://100.69.173.61:3001";
           http_addr = "100.69.173.61";
