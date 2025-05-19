16fbb3f06183641ea82ec8a214d0f95f8b61c7c0
lowerlee <minleekevin@gmail.com>
2025-05-18 17:04:38 -0700

    added flaresolverr running in podman container

--- modules/arr/flaresolverr.nix
+++ modules/arr/flaresolverr.nix
@@ -1,11 +1,24 @@
 { config, pkgs, ... }:
 
 {
-  services.flaresolverr = {
-    enable = true;
-    openFirewall = true;
-    port = 8191;
+  virtualisation.oci-containers = {
+    backend = "podman";
+    containers = {
+      flaresolverr = {
+        image = "ghcr.io/flaresolverr/flaresolverr:latest";
+        autoStart = true;
+        ports = [ "8191:8191" ];
+        environment = {
+          LOG_LEVEL = "info";
+          CAPTCHA_SOLVER = "none";
+          TZ = "America/Los_Angeles";
+        };
+        extraOptions = [
+          "--security-opt=no-new-privileges:true"
+        ];
+      };
+    };
   };
-  
+
   networking.firewall.allowedTCPPorts = [ 8191 ];
 }
