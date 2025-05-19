7c8b65ae5f071e71668736e12327a82bf57da70d
lowerlee <minleekevin@gmail.com>
2025-05-18 18:47:04 -0700

    added new git alias

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -17,17 +17,22 @@
       bash = {
         enable = true;
         shellAliases = {
+          # system alias
           build = "sudo nixos-rebuild switch --flake .";
+          nixos = "cd /etc/nixos";
+          snano = "sudo nano";
+          status = "sudo systemctl status";
+          log = "sudo journalctl -n 50 -u";
+
+          # git aliases
           add = "git add .";
           commit = "git commit -m";
           push = "git push origin";
           branch = "git branch";
           checkout = "git checkout";
           merge = "git merge";
-          nixos = "cd /etc/nixos";
-          snano = "sudo nano";
-          status = "sudo systemctl status";
-          log = "sudo journalctl -n 50 -u";
+          newbranch = "git checkout -b";
+          pushup = "git push -u origin";
         };
       };
       ssh = {
