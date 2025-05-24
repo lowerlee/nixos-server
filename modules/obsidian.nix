2ab36be5e25c60a9e90f0fb77ebdaa5129cec7af
lowerlee <minleekevin@gmail.com>
2025-05-24 15:06:46 -0700

    obsidian-remote

--- modules/obsidian.nix
+++ modules/obsidian.nix
@@ -30,35 +30,48 @@
     requires = [ "build-obsidian-remote.service" ];
   };
 
+  # Create custom startup script
+  systemd.tmpfiles.rules = [
+    "d /home/k/obsidian 0755 k users"
+    "d /home/k/obsidian/vaults 0755 k users"
+    "d /home/k/obsidian/config 0755 k users"
+    # Create custom startup script
+    "d /home/k/obsidian/scripts 0755 k users"
+  ];
+
+  environment.etc."obsidian-startup.sh" = {
+    text = ''
+      #!/bin/bash
+      # Custom startup script for Obsidian
+      export DISPLAY=:1
+      sleep 5  # Wait for desktop to load
+      /usr/bin/obsidian --no-sandbox &
+    '';
+    mode = "0755";
+  };
+
   virtualisation.oci-containers = {
     backend = "podman";
     containers = {
       obsidian-remote = {
-        image = "obsidian-remote:latest-git";  # Use locally built from main branch
+        image = "obsidian-remote:latest-git";
         autoStart = true;
-        ports = [ "8090:8080" ];  # Using 8090 to avoid potential conflicts
+        ports = [ "8090:8080" ];
         volumes = [
           "/home/k/obsidian/vaults:/vaults:Z"
           "/home/k/obsidian/config:/config:Z"
-          # Mount a custom startup script
-          "${pkgs.writeText "custom-autostart" ''
-            [Desktop Entry]
-            Type=Application
-            Name=Obsidian
-            Exec=/usr/bin/obsidian --no-sandbox
-            Hidden=false
-            NoDisplay=false
-            X-GNOME-Autostart-enabled=true
-          ''}:/home/abc/.config/autostart/obsidian.desktop:Z"
+          "/etc/obsidian-startup.sh:/usr/local/bin/obsidian-startup.sh:Z"
         ];
         environment = {
-          PUID = "1000";  # User ID for k (check with id command)
-          PGID = "100";   # Group ID for users (check with id command)
+          PUID = "1000";
+          PGID = "100";
           TZ = "America/Los_Angeles";
           DOCKER_MODS = "linuxserver/mods:universal-git";
-          # Auto-start Obsidian
           AUTO_UPDATES = "false";
           OBSIDIAN_ARGS = "--disable-gpu";
+          # Override the startup command
+          CUSTOM_USER = "abc";
+          AUTOSTART = "true";
         };
         extraOptions = [
           "--security-opt=no-new-privileges:true"
@@ -66,17 +79,12 @@
           "--device=/dev/dri"
           "--tmpfs=/tmp:noexec,nosuid,size=1g"
         ];
+        # Override the default command
+        cmd = [ "/bin/bash", "-c", "sleep 10 && /usr/local/bin/obsidian-startup.sh && exec /usr/local/bin/dockerd-entrypoint.sh" ];
       };
     };
   };
 
   # Open firewall port
   networking.firewall.allowedTCPPorts = [ 8090 ];
-
-  # Create required directories
-  systemd.tmpfiles.rules = [
-    "d /home/k/obsidian 0755 k users"
-    "d /home/k/obsidian/vaults 0755 k users"
-    "d /home/k/obsidian/config 0755 k users"
-  ];
 }
