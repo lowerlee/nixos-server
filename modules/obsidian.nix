0629db362decc01b70a0d838b30fe62fe20805da
lowerlee <minleekevin@gmail.com>
2025-05-24 14:33:05 -0700

    obsidian-remote

--- modules/obsidian.nix
+++ modules/obsidian.nix
@@ -1,37 +1,45 @@
 { config, pkgs, ... }:
 
 {
+  # Create necessary directories
+  systemd.tmpfiles.rules = [
+    "d /var/lib/obsidian-remote 0755 k users"
+    "d /var/lib/obsidian-remote/config 0755 k users"
+  ];
+
   virtualisation.oci-containers = {
     backend = "podman";
     containers = {
       obsidian-remote = {
-        image = "ghcr.io/sytone/obsidian-remote:1.5.3";
+        image = "ghcr.io/sytone/obsidian-remote:latest";
         autoStart = true;
-        ports = [ "8090:8080" ];
+        ports = [ 
+          "8080:8080"
+          "8443:8443" 
+        ];
         volumes = [
-          "/home/k/obsidian/vaults:/vaults:Z"
-          "/home/k/obsidian/config:/config:Z"
+          # Use your existing Syncthing obsidian folder as the vault
+          "/home/k/obsidian:/vaults:Z"
+          "/var/lib/obsidian-remote/config:/config:Z"
         ];
         environment = {
           PUID = "1000";
-          PGID = "100";
+          PGID = "1000";
           TZ = "America/Los_Angeles";
+          DOCKER_MODS = "linuxserver/mods:universal-git";
+          CUSTOM_PORT = "8080";
+          CUSTOM_HTTPS_PORT = "8443";
+          # Optional: Set username/password for authentication
+          # CUSTOM_USER = "your_username";
+          # PASSWORD = "your_password";
         };
         extraOptions = [
           "--security-opt=no-new-privileges:true"
-          "--shm-size=1gb"
         ];
       };
     };
   };
 
-  # Open firewall port
-  networking.firewall.allowedTCPPorts = [ 8090 ];
-
-  # Create required directories
-  systemd.tmpfiles.rules = [
-    "d /home/k/obsidian 0755 k users"
-    "d /home/k/obsidian/vaults 0755 k users"
-    "d /home/k/obsidian/config 0755 k users"
-  ];
+  # Open firewall ports
+  networking.firewall.allowedTCPPorts = [ 8080 8443 ];
 }
