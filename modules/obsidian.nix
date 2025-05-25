a879c8412b09945f7baf24f1f3f4a283c565918b
lowerlee <minleekevin@gmail.com>
2025-05-24 19:54:28 -0700

    obsidian

--- modules/obsidian.nix
+++ modules/obsidian.nix
@@ -16,11 +16,11 @@
         proxyPass = "http://localhost:8090/";
         extraConfig = ''
           # Fix for Tailscale funnel proxy headers
-          proxy_set_header Host $http_host;
+          proxy_set_header Host $host;
           proxy_set_header X-Real-IP $remote_addr;
           proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
           proxy_set_header X-Forwarded-Proto https;
-          proxy_set_header X-Forwarded-Host $http_host;
+          proxy_set_header X-Forwarded-Host $host;
           
           # Prevent redirect loops
           proxy_redirect off;
@@ -50,7 +50,7 @@
       locations."~ ^/obsidian/(api|static|css|js|img|fonts|media|audio|public|vnc)/" = {
         proxyPass = "http://localhost:8090";
         extraConfig = ''
-          proxy_set_header Host $http_host;
+          proxy_set_header Host $host;
           proxy_set_header X-Real-IP $remote_addr;
           proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
           proxy_set_header X-Forwarded-Proto https;
